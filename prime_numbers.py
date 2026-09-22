6ebfeab2c6490df620790e0cef1039af067b707e
정승현 <jsh635607@gmail.com>
2026-09-22 17:35:59 +0900

    feat: math.sqrt를 이용한 소수 판별 연산 최적화

--- prime_numbers.py
+++ prime_numbers.py
@@ -1,9 +1,12 @@
+# prime-number.py - 제곱근을 이용한 속도 최적화 버전
 import math
 
-print("1부터 100 사이의 소수(최적화 버전):")
+print("1부터 100 사이의 소수 (최적화 버전):")
+
 for num in range(2, 101):
     is_prime = True
 
+    # 제곱근까지만 확인
     for i in range(2, int(math.sqrt(num)) + 1):
         if num % i == 0:
             is_prime = False
